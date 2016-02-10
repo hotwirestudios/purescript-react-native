73aceb581fdee4adf52b73221b54d05fe8a51712
Fabian Muecke <fabian@hotwirestudios.com>
2016-02-10 14:35:16 +0100

    Added colorWithAlpha

--- src/ReactNative.purs
+++ src/ReactNative.purs
@@ -8,3 +8,6 @@ type AppKey = String
 foreign import registerComponent :: forall e props. AppKey -> ReactClass props -> Eff e Unit
 
 newtype Color = Color String
+
+colorWithAlpha :: Color -> String -> Color
+colorWithAlpha (Color color) string = Color $ color ++ string
