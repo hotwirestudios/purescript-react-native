dd1c24f94a26dc3fe7e1e1995442a745458e41ff
Fabian Muecke <fabian@hotwirestudios.com>
2016-02-12 17:28:49 +0100

    Added Animated.Value

--- src/ReactNative/Animated.js
+++ src/ReactNative/Animated.js
@@ -3,3 +3,8 @@
 // module ReactNative.Animated
 
 exports.animatedViewClass = require('react-native').Animated.View;
+
+var AnimatedValue = require('react-native').Animated.Value;
+exports.animatedValue = function(param) {
+    return new AnimatedValue(param);
+};
--- src/ReactNative/Animated.purs
+++ src/ReactNative/Animated.purs
@@ -1,21 +1,21 @@
 module ReactNative.Animated where
 
+import Prelude (Unit)
 import React (ReactClass, ReactElement)
 import React.DOM.Props (Props)
 import ReactNative.Components (createNativeElement)
 
--- foreign import data AnimatedImplementation :: * -> *
-foreign import data AnimatedInterpolation :: *
-
 foreign import animatedViewClass :: forall props. ReactClass props
+foreign import animatedValue :: forall a. a -> AnimatedImplementation a
 
 type InterpolationConfigType = {
     inputRange :: Array Number,
     outputRange :: Array Number
 }
 
-type AnimatedImplementation = {
-    interpolate :: InterpolationConfigType -> Number
+type AnimatedImplementation a = {
+    interpolate :: InterpolationConfigType -> a,
+    setValue :: a -> Unit
 }
 
 animatedView :: Array Props -> Array ReactElement -> ReactElement
