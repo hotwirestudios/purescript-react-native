54a965f61754a5b1492cbaed4da6e865a8a01073
Fabian Muecke <fabian@hotwirestudios.com>
2016-03-30 11:18:46 +0200

    ReactNative.Props.Android can now be imported safely on iOS

--- src/ReactNative/Props.android.js
+++ src/ReactNative/Props.android.js
@@ -4,15 +4,24 @@
 var TouchableNativeFeedback = require('react-native').TouchableNativeFeedback;
 
 exports.selectableBackground_ = function () {
+    if (TouchableNativeFeedback === undefined || TouchableNativeFeedback.SelectableBackground === undefined) {
+        return null;
+    }
     return TouchableNativeFeedback.SelectableBackground();
 };
 
 exports.selectableBackgroundBorderless_ = function () {
+    if (TouchableNativeFeedback === undefined || TouchableNativeFeedback.SelectableBackgroundBorderless === undefined) {
+        return null;
+    }
     return TouchableNativeFeedback.SelectableBackgroundBorderless();
 };
 
 exports.ripple_ = function (color) {
     return function (borderless) {
+        if (TouchableNativeFeedback === undefined || TouchableNativeFeedback.Ripple === undefined) {
+            return null;
+        }
         return TouchableNativeFeedback.Ripple(color, borderless);
     };
 };
