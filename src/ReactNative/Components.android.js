24d352e10124e7659af5df27f0823ee69127cbc1
Fabian Muecke <fabian@hotwirestudios.com>
2016-04-01 10:57:26 +0200

    Added ProgressBarAndroid

--- src/ReactNative/Components.android.js
+++ src/ReactNative/Components.android.js
@@ -5,6 +5,7 @@
 exports.touchableNativeFeedbackClass = require('react-native').TouchableNativeFeedback;
 exports.viewPagerAndroidClass = require('react-native').ViewPagerAndroid;
 exports.toolbarAndroidClass = require('react-native').ToolbarAndroid;
+exports.progressBarAndroidClass = require('react-native').ProgressBarAndroid;
 
 exports.setPage = function(viewPager) {
     return function (pageIndex) {
--- src/ReactNative/Components.android.purs
+++ src/ReactNative/Components.android.purs
@@ -1,9 +1,11 @@
 module ReactNative.Components.Android where
 
-import Prelude (Unit)
+import Prelude (Unit, (<>), ($))
 import Control.Monad.Eff (Eff)
+import Data.Maybe (Maybe(Just, Nothing))
 import React (ReactElement, ReactClass)
-import React.DOM.Props (Props)
+import React.DOM.Props (Props, unsafeMkProps)
+import ReactNative (Color, colorToString)
 import ReactNative.Components (createNativeElement)
 
 foreign import data PageChange :: !
@@ -12,6 +14,7 @@ foreign import touchableNativeFeedbackClass :: forall props. ReactClass props
 foreign import viewPagerAndroidClass :: forall props. ReactClass props
 foreign import setPage :: forall a eff. a -> Int -> Eff (page :: PageChange | eff) Unit
 foreign import toolbarAndroidClass :: forall props. ReactClass props
+foreign import progressBarAndroidClass :: forall props. ReactClass props
 
 touchableNativeFeedback :: Array Props -> ReactElement -> ReactElement
 touchableNativeFeedback props element = createNativeElement touchableNativeFeedbackClass props [element]
@@ -21,3 +24,50 @@ viewPagerAndroid = createNativeElement viewPagerAndroidClass
 
 toolbarAndroid :: Array Props -> ReactElement
 toolbarAndroid props = createNativeElement toolbarAndroidClass props []
+
+type ProgressBarAndroidProps =
+    { progress :: Maybe Number
+    , indeterminate :: Maybe Boolean
+    , color :: Maybe Color
+    , styleAttr :: Maybe ProgressBarAndroidStyleAttribute
+    }
+
+data ProgressBarAndroidStyleAttribute = ProgressBarAndroidStyleAttributeHorizontal | ProgressBarAndroidStyleAttributeNormal | ProgressBarAndroidStyleAttributeSmall | ProgressBarAndroidStyleAttributeLarge | ProgressBarAndroidStyleAttributeInverse | ProgressBarAndroidStyleAttributeSmallInverse | ProgressBarAndroidStyleAttributeLargeInverse
+
+progressBarAndroidProps :: ProgressBarAndroidProps
+progressBarAndroidProps =
+    { progress: Nothing
+    , indeterminate: Nothing
+    , color: Nothing
+    , styleAttr: Nothing
+    }
+
+progressBarAndroid :: ProgressBarAndroidProps -> Array Props -> ReactElement
+progressBarAndroid barProps props = createNativeElement progressBarAndroidClass combinedProps []
+    where
+        combinedProps = props <>
+            case barProps.progress of
+                Nothing -> []
+                Just progress -> [unsafeMkProps "progress" progress]
+            <>
+            case barProps.indeterminate of
+                Nothing -> []
+                Just indeterminate -> [unsafeMkProps "indeterminate" indeterminate]
+            <>
+            case barProps.color of
+                Nothing -> []
+                Just color -> [unsafeMkProps "color" $ colorToString color]
+            <>
+            case barProps.styleAttr of
+                Nothing -> []
+                Just styleAttr ->
+                    [ unsafeMkProps "styleAttr" $
+                        case styleAttr of
+                            ProgressBarAndroidStyleAttributeHorizontal -> "Horizontal"
+                            ProgressBarAndroidStyleAttributeNormal -> "Normal"
+                            ProgressBarAndroidStyleAttributeSmall -> "Small"
+                            ProgressBarAndroidStyleAttributeLarge -> "Large"
+                            ProgressBarAndroidStyleAttributeInverse -> "Inverse"
+                            ProgressBarAndroidStyleAttributeSmallInverse -> "SmallInverse"
+                            ProgressBarAndroidStyleAttributeLargeInverse -> "LargeInverse"
+                    ]
