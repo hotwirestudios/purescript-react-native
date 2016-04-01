180602fc86c7f99e01f4692f92a6ec43ca13affd
Fabian Muecke <fabian@hotwirestudios.com>
2016-04-01 21:10:51 +0200

    Added LayoutAnimation

--- src/ReactNative/Animated.js
+++ src/ReactNative/Animated.js
@@ -8,3 +8,23 @@ var AnimatedValue = require('react-native').Animated.Value;
 exports.animatedValue = function(param) {
     return new AnimatedValue(param);
 };
+
+var LayoutAnimation = require('react-native').LayoutAnimation;
+exports.spring = function() {
+    return LayoutAnimation.spring();
+};
+exports.linear = function() {
+    return LayoutAnimation.linear();
+};
+exports.easeInEaseOut = function() {
+    return LayoutAnimation.easeInEaseOut();
+};
+exports.easeIn = function() {
+    return LayoutAnimation.easeIn();
+};
+exports.easeOut = function() {
+    return LayoutAnimation.easeOut();
+};
+exports.keyboard = function() {
+    return LayoutAnimation.keyboard();
+};
--- src/ReactNative/Animated.purs
+++ src/ReactNative/Animated.purs
@@ -1,6 +1,7 @@
 module ReactNative.Animated where
 
 import Prelude (Unit)
+import Control.Monad.Eff (Eff)
 import React (ReactClass, ReactElement)
 import React.DOM.Props (Props)
 import ReactNative.Components (createNativeElement)
@@ -20,3 +21,24 @@ type AnimatedImplementation a = {
 
 animatedView :: Array Props -> Array ReactElement -> ReactElement
 animatedView = createNativeElement animatedViewClass
+
+foreign import data Animation :: !
+
+data LayoutAnimation = Spring | Linear | EaseInEaseOut | EaseIn | EaseOut | Keyboard
+
+foreign import spring :: forall eff. Eff (animation :: Animation | eff) Unit
+foreign import linear :: forall eff. Eff (animation :: Animation | eff) Unit
+foreign import easeInEaseOut :: forall eff. Eff (animation :: Animation | eff) Unit
+foreign import easeIn :: forall eff. Eff (animation :: Animation | eff) Unit
+foreign import easeOut :: forall eff. Eff (animation :: Animation | eff) Unit
+foreign import keyboard :: forall eff. Eff (animation :: Animation | eff) Unit
+
+animateLayoutChanges :: forall eff. LayoutAnimation -> Eff (animation :: Animation | eff) Unit
+animateLayoutChanges anim =
+    case anim of
+        Spring -> spring
+        Linear -> linear
+        EaseInEaseOut -> easeInEaseOut
+        EaseIn -> easeIn
+        EaseOut -> easeOut
+        Keyboard -> keyboard
